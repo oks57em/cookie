f693e8ff621dedb1e13f27cab1dd7dc1a559eaa5
oks57em <125505560+oks57em@users.noreply.github.com>
2023-03-01 17:57:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// на вход число которое выдает четное или нет(делится ли оно на 2 без остатка)
+//4 ->да
+//-3->нет
+//7 ->нет
+
+Console.WriteLine("ввести первое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 ==0)
+{
+    Console.WriteLine("остатка нет.Все верно");
+}
+else
+{
+    Console.WriteLine("Это нечетное число");
+}
