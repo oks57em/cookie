d83fd0de56f7a69d7b481f080bf35d425340c934
oks57em <125505560+oks57em@users.noreply.github.com>
2023-03-01 18:06:45 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
-﻿// на вход три числа и выдает максимальное
-//2,3,7 ->7
-//44,5,78 ->78
-//22,3,9 ->22
+﻿// на вход число которое выдает четное или нет(делится ли оно на 2 без остатка)
+//4 ->да
+//-3->нет
+//7 ->нет
+
 Console.WriteLine("ввести первое число:");
-int numbera = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("ввести второе число:");
-int numberb = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("ввести третье число:");
-int numberc = Convert.ToInt32(Console.ReadLine());
-int max = numbera;
-if (numbera>max) max = numbera;
-if (numberb>max) max = numberb;
-if (numberc>max) max = numberc;
-Console.Write("max =");
-Console.WriteLine(max);
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 ==0)
+{
+    Console.WriteLine("остатка нет.Все верно");
+}
+else
+{
+    Console.WriteLine("Это нечетное число");
+}
